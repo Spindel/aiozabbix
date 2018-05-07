4e7d39b4b4ce324aaa1a791a785d718e2477f61c
Nicklas Lindgren <nili@gulmohar.se>
2018-05-07 14:14:40 +0200

    Implement asynchronous version of the pyzabbix library
    This version also attempts to re-login if there is an authentication
    error.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import (
+    find_packages,
+    setup,
+)
+
+setup(
+    name='aiozabbix',
+    version='0.0.1',
+    packages=find_packages(),
+    install_requires=[
+        'jsonrpcclient[aiohttp]',
+    ]
+)
