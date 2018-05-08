e237a0ee434399c33b41175b417c0ebc6f61cf8b
Nicklas Lindgren <nili@modio.se>
2018-05-08 14:26:15 +0200

    Add README.md, more package metadata

--- setup.py
+++ setup.py
@@ -1,12 +1,22 @@
-from setuptools import (
-    find_packages,
-    setup,
-)
+from setuptools import setup
 
 setup(
     name='aiozabbix',
     version='0.0.1',
-    packages=find_packages(),
+    description='Asynchronous Zabbix API Python interface',
+    url='http://gitlab.com/ModioAB/aiozabbix',
+    author='Modio AB',
+    author_email='nili@modio.se',
+    license='LGPL',
+    classifiers=[
+        'Framework :: AsyncIO',
+        'License :: OSI Approved :: GNU Lesser General Public License v2 or later (LGPLv2+)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords='zabbix monitoring api',
+    packages=['aiozabbix'],
+    python_requires='>=3.6',
     install_requires=[
         'aiohttp',
     ],
