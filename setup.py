89c56fe0c4d1219fb3b0d1050f2f1de587b661e6
Nicklas Lindgren <nili@modio.se>
2019-12-16 16:44:20 +0100

    Bump version to 1.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='aiozabbix',
-    version='1.0.0',
+    version='1.1.0',
     description='Asynchronous Zabbix API Python interface',
     url='http://gitlab.com/ModioAB/aiozabbix',
     author='Modio AB',
